631b8325cf4e74dc8d566a928a528aaf31750641
蘇銘凱 <sjs47311@gmail.com>
2024-11-20 19:37:56 +0800

    調整FlexMessage已讀後進行回傳api 20

--- line_bot_project/bot/models.py
+++ line_bot_project/bot/models.py
@@ -13,6 +13,7 @@ class UserTag(models.Model):
     user_id = models.CharField(max_length=50)
     tag_name = models.CharField(max_length=100)
     tagged_at = models.DateTimeField(auto_now_add=True)
+    extra_data = models.JSONField(default=dict, blank=True)  # 新增欄位
 
     class Meta:
         db_table = 'user_tags'
