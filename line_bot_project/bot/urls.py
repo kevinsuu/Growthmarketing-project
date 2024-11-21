6b192be0548631d56cf7b17987cd985378bdd937
蘇銘凱 <sjs47311@gmail.com>
2024-11-21 10:58:22 +0800

    調整FlexMessage已讀後進行回傳api 71

--- line_bot_project/bot/urls.py
+++ line_bot_project/bot/urls.py
@@ -14,5 +14,5 @@ urlpatterns = [
     path('narrowcast/', NarrowcastMessageView.as_view(), name='narrowcast'),
     path('web/', TagStatsView.as_view(), name='tag_stats'),  # 新的URL路徑
     path('api/push-message/', PushMessageView.as_view(), name='push_message'),
-    path('message-statistics/<str:tracking_id>/', MessageStatisticsView.as_view(), name='message-statistics'),
+    path('message-statistics/<str:tracking_id>', MessageStatisticsView.as_view(), name='message-statistics'),
 ]
