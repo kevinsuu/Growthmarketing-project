f5b674bf33a60d692cda459494003c70c11754aa
蘇銘凱 <sjs47311@gmail.com>
2024-11-21 14:17:07 +0800

    調整FlexMessage已讀後進行回傳api 9

--- line_bot_project/bot/views.py
+++ line_bot_project/bot/views.py
@@ -62,8 +62,11 @@ class LineWebhookView(View):
         def handle_postback(event):
             data = dict(parse_qsl(event.postback.data))
             action = data.get('action')
-            user_id = event.source.user_id
-
+            # user_id = event.source.user_id
+            user_id = 'narrowcast_message'
+                
+            logger.info(f"user_id: {user_id}")
+            logger.info(f"action: {action}")
             # 追蹤按鈕點擊
             click_result = self.line_service.track_message_click(user_id, action)
             
